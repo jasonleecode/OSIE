6e526f7f97fb3e43435039d99c909968107bc7b1
Ivan Tyagov <ivan@nexedi.com>
2020-06-22 16:00:36 +0300

    Add notes for slapos / re6stnet setup for debian 10.

--- examples/lime2-mod-io-relay-on_off.py
+++ examples/lime2-mod-io-relay-on_off.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """
 This is an example of connecting LIME2 + LIME2-SHIELD + MOD-IO (over UEXT) and controlling a relay (on / off).
+It's mixed with slapos / re6stnet install.
 
 
 I. prepare lime2 + lime2's shield + MOD-IO
 
-# install packages
+# install packages at your laptop
 root@t470:~/tmp$ apt install minicom
 
-ivan@t470:~/tmp$ wget http://images.olimex.com/release/a20/A20-OLinuXino-bionic-minimal-20200601-122736.img.7z
-ivan@t470:~/tmp$ 7z e A20-OLinuXino-bionic-minimal-20200601-122736.img.7z 
+# XXX: currently nexedi suports fro armhf only debian (slapos + re6stnet packages)
+ivan@t470:~/tmp$ wget http://images.olimex.com/release/a20/A20-OLinuXino-buster-minimal-20200601-122725.img.7z
+ivan@t470:~/tmp$ 7z e A20-OLinuXino-buster-minimal-20200601-122725.img.7z
+
 # download from https://www.balena.io/etcher/
 ivan@t470:~/tmp$ unzip ../balena-etcher-electron-1.5.95-linux-x64.zip
 ivan@t470:~/tmp$ chmod o+x balenaEtcher-1.5.95-x64.AppImage
@@ -57,6 +60,22 @@ root@a20-olinuxino:~# i2cdetect -r 1  # should show  0х58 is available
 
 
 II. finally run this script as root, you should see RELAY1's led showing green color.
+
+III. Integration with Nexedi's toolset for Wendelin 
+
+    # re6snet
+    root@a20-olinuxino:~# apt install htop tig git etckeeper vim
+    root@a20-olinuxino:~# wget https://deploy.erp5.net/gnet/re6st && bash re6st # get re6stnet token from slapos.vifib.com 
+    root@a20-olinuxino:~# echo "default" >> /etc/re6stnet/re6stnet.conf
+    root@a20-olinuxino:~# /etc/init.d/re6stnet restart
+
+    # slapos
+    root@a20-olinuxino:~# wget https://deploy.erp5.net/slapos && bash slapos # get slapos token from slapos.vifib.com
+    
+    # fluentd 
+    ivan@t470:~$ slapos console
+    >>> release="https://lab.nexedi.com/nexedi/slapos/raw/master/software/fluentd/software.cfg"
+    >>> supply(release, "COMP-3217")
 
 """
 

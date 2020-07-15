4e35ae4d2ff35bce8f594afa84f1dea79847c448
Ivan Tyagov <ivan@nexedi.com>
2020-07-15 10:46:31 +0000

    Multiple coils case.

--- examples/modbus/modbus-client.py
+++ examples/modbus/modbus-client.py
@@ -2,6 +2,13 @@ from pymodbus.client.sync import ModbusTcpClient
 
 client = ModbusTcpClient('localhost')
 client.write_coil(1, True)
+client.write_coil(2, False)
+
 result = client.read_coils(1,1)
 print(result.bits[0])
+
+result = client.read_coils(2,1)
+print(result.bits[0])
+
+
 client.close()

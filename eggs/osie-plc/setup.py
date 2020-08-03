0aa82b37e53da8af8692e87bea12cc7307e4bd5f
Ivan Tyagov <ivan@nexedi.com>
2020-08-03 12:38:11 +0000

    Add numpy.

--- eggs/osie-plc/setup.py
+++ eggs/osie-plc/setup.py
@@ -7,5 +7,5 @@ setup(
     name='osie-plc',
     version='0.1',
     #entry_points={'console_scripts': ['windea = windea:main']},
-    install_requires=['pyA20Lime2', 'pymodbus[twisted]', 'click', 'prompt_toolkit', 'pygments' ]
+    install_requires=['pyA20Lime2', 'pymodbus[twisted]', 'click', 'prompt_toolkit', 'pygments', 'numpy' ]
 )

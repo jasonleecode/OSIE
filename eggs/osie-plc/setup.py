a5f3662329814de8a622b5c9d09ae67dd3285174
Ivan Tyagov <ivan@nexedi.com>
2020-08-04 11:46:08 +0000

    Generate modbus_server console script. Remove numpy as NOT strictly needed for PLC's work.

--- eggs/osie-plc/setup.py
+++ eggs/osie-plc/setup.py
@@ -6,6 +6,6 @@ from setuptools import setup
 setup(
     name='osie-plc',
     version='0.1',
-    #entry_points={'console_scripts': ['windea = windea:main']},
-    install_requires=['pyA20Lime2', 'pymodbus[twisted]', 'click', 'prompt_toolkit', 'pygments', 'numpy' ]
+    entry_points={'console_scripts': ['modbus_server = modbus_server:main']},
+    install_requires=['pyA20Lime2', 'pymodbus[twisted]', 'click', 'prompt_toolkit', 'pygments']
 )

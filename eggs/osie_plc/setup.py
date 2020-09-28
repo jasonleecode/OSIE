20d0db887afe627518cc8b5ba054282269ef356e
Ivan Tyagov <ivan@nexedi.com>
2020-09-28 11:24:01 +0000

    Rename to keep convention.

--- eggs/osie_plc/setup.py
+++ eggs/osie_plc/setup.py
@@ -4,8 +4,8 @@ Install OSIE-PLC.
 from setuptools import setup
 
 setup(
-    name='osie-plc',
+    name='osie_plc',
     version='0.1',
-    entry_points={'console_scripts': ['modbus_server = modbus_server:main']},
+    entry_points={'console_scripts': ['pymodbus_server = modbus_server:main']},
     install_requires=['pyA20Lime2', 'pymodbus[twisted]', 'click', 'prompt_toolkit', 'pygments']
 )

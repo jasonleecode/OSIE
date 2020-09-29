7045b3865c743663c860f80f86b89f98eef4b1e3
Ivan Tyagov <ivan@nexedi.com>
2020-09-29 12:54:27 +0000

    Add needed dependency.

--- eggs/osie_plc/setup.py
+++ eggs/osie_plc/setup.py
@@ -7,5 +7,5 @@ setup(
     name='osie_plc',
     version='0.1',
     entry_points={'console_scripts': ['pymodbus_server = modbus_server:main']},
-    install_requires=['pyA20Lime2', 'pymodbus[twisted]', 'click', 'prompt_toolkit', 'pygments']
+    install_requires=['pyA20Lime2', 'pymodbus[twisted]', 'click', 'prompt_toolkit', 'pygments', 'bitarray']
 )

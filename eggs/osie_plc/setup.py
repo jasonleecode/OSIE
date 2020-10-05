cbc88c45dced2831cd4cb00e431e9a38d74b56c6
Ivan Tyagov <ivan@nexedi.com>
2020-10-05 11:50:06 +0000

    Adjust setup of egg after rename.

--- eggs/osie_plc/setup.py
+++ eggs/osie_plc/setup.py
@@ -6,6 +6,6 @@ from setuptools import setup
 setup(
     name='osie_plc',
     version='0.1',
-    entry_points={'console_scripts': ['pymodbus_server = osie_plc.modbus_server:main']},
+    entry_points={'console_scripts': ['osie_plc_server = osie_plc.osie_plc:main']},
     install_requires=['pyA20Lime2', 'pymodbus[twisted]', 'click', 'prompt_toolkit', 'pygments', 'bitarray', 'cython', 'numpy', 'pandas']
 )

f5bd161d1ad07e0162e933f63415b0bf7071f97c
Ivan Tyagov <ivan@nexedi.com>
2021-05-28 14:19:58 +0000

    Typo.

--- eggs/osie_coupler/setup.py
+++ eggs/osie_coupler/setup.py
@@ -6,6 +6,6 @@ from setuptools import setup
 setup(
     name='osie_coupler',
     version='0.1',
-    entry_points={'console_scripts': ['osie_plc_server = osie_coupler.osie_coupler:main']},
+    entry_points={'console_scripts': ['osie_coupler_server = osie_coupler.osie_coupler:main']},
     install_requires=['pyA20Lime2', 'pymodbus[twisted]', 'click', 'prompt_toolkit', 'pygments', 'bitarray']
 )

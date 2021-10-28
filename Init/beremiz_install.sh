1683c57422ffda4cb3e608744b4467ac5c3137ec
baa <balakrishna.balakrishna@fokus.fraunhofer.de>
2021-10-28 15:48:52 +0200

    Inclusion of open62541 installation file

---
+++ Init/beremiz_install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# =================================================
+#
+#        Beremiz installation in Ubuntu 20.04
+#
+# =================================================
+
+
+# run as root
+if [[ $EUID -ne 0 ]]; then
+   echo 'run this script as sudo !' 1>&2
+   exit 1
+fi
+
+echo "OSTYPE:"
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+   ls -al /etc/*-release
+   cat /etc/arch-release
+
+   # Ubuntu
+   if [ -f /etc/os-release ]; then
+      . /etc/os-release
+      OS_NAME=$NAME
+      OS_VER_ID=$VERSION_ID
+      echo "System OS_NAME: $OS_NAME"
+
+   else
+      echo "tested release files do not exist -> happens with older or unsupported OS versions"
+      exit 2
+   fi
+
+   echo "Found OS information: $OS_NAME, version: $OS_VER_ID"
+
+   if [[ "$OS_NAME" == *"Ubuntu"* ]]; then
+      echo "Ubuntu"
+      export DEBIAN_FRONTEND=noninteractive
+
+      # Update system
+      # install required packages
+      apt -yq update
+      apt -yq install python-wxgtk3.0 python-lxml python-is-python2 python-numpy build-essential python-dev-is-python2 mercurial autoconf bison flex python2 virtualenv python-lxml python-numpy libgtk-3-dev libgl1-mesa-dev libglu1-mesa-dev
+      pip2 install pyro twisted nevow autobahn lxml opcua
+
+      # Path preparation
+      LOCAL_HOME_DIR=/home/${SUDO_USER:-$(whoami)}/
+      echo "$LOCAL_HOME_DIR"
+
+      # Build Beremiz
+      echo "Building Beremiz."
+      cd $LOCAL_HOME_DIR/
+      mkdir -p Beremiz
+      cd Beremiz/
+      hg clone https://hg.beremiz.org/beremiz
+
+      # Build Matiec
+      echo "Building Matiec."
+      hg clone https://hg.beremiz.org/matiec
+      autoreconf -i
+      ./configure
+      make -j
+      pwd
+
+      # Build Modbus
+      echo "Building Modbus."  
+      hg clone https://hg.beremiz.org/Modbus/
+      make
+
+      echo "Beremiz is installed." 
+      echo "Run $ python2 ~/Beremiz/Beremiz/Beremiz.py to open Beremiz" 
+
+
+   else 
+      echo "Distro is not supported."
+      exit 3
+   fi
+else
+   echo "ERROR: OS is not supported: $OSTYPE"
+   exit 4
+fi
--- Init/open62541_install.sh
+++ Init/open62541_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # =================================================
 #
-#        Beremiz installation in Ubuntu 20.04
+#        Open62541 installation in Ubuntu 20.04
 #
 # =================================================
 
@@ -36,42 +36,30 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
       echo "Ubuntu"
       export DEBIAN_FRONTEND=noninteractive
 
-      # Update system
-      # install required packages
-      apt -yq update
-      apt -yq install python-wxgtk3.0 python-lxml python-is-python2 python-numpy build-essential python-dev-is-python2 mercurial autoconf bison flex python2 virtualenv python-lxml python-numpy libgtk-3-dev libgl1-mesa-dev libglu1-mesa-dev
-      pip2 install pyro twisted nevow autobahn lxml opcua
-
       # Path preparation
       LOCAL_HOME_DIR=/home/${SUDO_USER:-$(whoami)}/
       echo "$LOCAL_HOME_DIR"
 
-      # Build Beremiz
-      echo "Building Beremiz."
+      # Build and install open62541
+      echo "Building and installing open62541."
       cd $LOCAL_HOME_DIR/
-      mkdir -p Beremiz
-      cd Beremiz/
-      hg clone https://hg.beremiz.org/beremiz
-
-      # Build Matiec
-      echo "Building Matiec."
-      hg clone https://hg.beremiz.org/matiec
-      autoreconf -i
-      ./configure
+      git clone https://github.com/open62541/open62541.git
+      cd open62541/
+      git submodule update --init --recursive
+      mkdir -p build && cd build
+      cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUA_NAMESPACE_ZERO=FULL -DUA_ENABLE_AMALGAMATION=ON ..
       make -j
-      pwd
+      make install
 
-      # Build Modbus
-      echo "Building Modbus."  
-      hg clone https://hg.beremiz.org/Modbus/
-      make
-
+      echo "open62541 is installed." 
+      echo "Transfer open62541.c and open62541.h to your project location from the buld directory"
+      echo "Ex: cp ~/open62541/build/open62541.* ~/osie/coupler/opc-ua-server/"
 
    else 
-      echo "Distro not supported."
+      echo "Distro is not supported."
       exit 3
    fi
 else
-   echo "ERROR: OS not supported: $OSTYPE"
+   echo "ERROR: OS is not supported: $OSTYPE"
    exit 4
 fi

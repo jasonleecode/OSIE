a48d76c0c61c21d1350c7c3059ae103cf4130f31
Ivan Tyagov <ivan@nexedi.com>
2021-12-03 14:39:54 +0200

    Add skeleton for testing.

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,10 @@
+"""
+  OPC-UA test agent used for testing of PLC program here: https://lab.nexedi.com/nexedi/osie/tree/master/Beremiz/beremiz_test_opc_ua
+  XXX:
+  - add asserts
+"""
 from opcua import Client
+import time
 
 client = Client("opc.tcp://k2-osie:4840/")
 
@@ -8,9 +14,13 @@ try:
   # Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
   root = client.get_root_node()
   children_list = root.get_children()
-
-  var = client.get_node("ns=1;s=i2c0.relay0")
-  i2c0_relay0 = var.get_value()
-  print  i2c0_relay0
+  for i in range (0,10):
+    var = client.get_node("ns=1;s=i2c0.relay0")
+    i2c0_relay0 = var.get_value()
+    # XXX: do asserts here
+    print  i2c0_relay0
+    print "sleep"
+    time.sleep(5)
+  
 finally:
   client.disconnect()

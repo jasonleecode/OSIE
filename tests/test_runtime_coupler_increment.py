8087649874862d0c85e4d7740c9fde79e9068c87
Ivan Tyagov <ivan@nexedi.com>
2021-12-08 10:02:50 +0200

    Pass paramaters from CLI.

--- tests/test_runtime_coupler_increment.py
+++ tests/test_runtime_coupler_increment.py
@@ -4,32 +4,60 @@
 """
 from opcua import Client
 import time
+import argparse
+import configparser
 
-TIMEOUT = 5 # seconds to wait between checks
-NUMBER_OF_CHECKS = 10 # number of times we check the server
-OPC_UA_ADDRESS = "opc.tcp://k2-osie:4840/"
-OPC_UA_IDENTIFIER = "ns=1;s=i2c0.relay0"
+def main():
 
-# connect to a session at OPC-UA server
-client = Client(OPC_UA_ADDRESS)
+  # handle CLI arguments
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--iterations', \
+                      type = int, \
+                      default = 10, \
+                      help='number of iterations to check')
+  parser.add_argument('--timeout', \
+                      type = int, \
+                      default = 5, \
+                      help='seconds to wait between checks')
+  parser.add_argument('--opc-ua-server', \
+                      type = str, \
+                      default = 'opc.tcp://k2-osie:4840/', \
+                      help='Address of OPC-UA server')
+  parser.add_argument('--opc-ua-node-identifier', \
+                      type = str, \
+                      default = 'ns=1;s=i2c0.relay0', \
+                      help='Address of OPC-UA server')
 
-try:
-  client.connect()
+  args = parser.parse_args()
+  NUMBER_OF_CHECKS = args.iterations
+  TIMEOUT  = args.timeout
+  OPC_UA_ADDRESS = args.opc_ua_server
+  OPC_UA_IDENTIFIER = args.opc_ua_node_identifier
 
-  # Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
-  root = client.get_root_node()
-  children_list = root.get_children()
-  var = client.get_node(OPC_UA_IDENTIFIER)
 
-  for i in range (0, NUMBER_OF_CHECKS):
-    i2c0_relay0_before = var.get_value()
-    print("\ni2c0_relay0 (before) = ", i2c0_relay0_before)
-    print("sleep ...")
-    time.sleep(TIMEOUT)
-    i2c0_relay0_after = var.get_value()
-    print("i2c0_relay0 (after) = ", i2c0_relay0_after)
-    # for the wait timeout runtime should have increased the value
-    assert i2c0_relay0_after > i2c0_relay0_before
+  # connect to a session at OPC-UA server
+  client = Client(OPC_UA_ADDRESS)
 
-finally:
-  client.disconnect()
+  try:
+    client.connect()
+
+    # Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
+    root = client.get_root_node()
+    children_list = root.get_children()
+    var = client.get_node(OPC_UA_IDENTIFIER)
+
+    for i in range (0, NUMBER_OF_CHECKS):
+      i2c0_relay0_before = var.get_value()
+      print("\ni2c0_relay0 (before) = ", i2c0_relay0_before)
+      print("Sleep for %s seconds ..." %TIMEOUT)
+      time.sleep(TIMEOUT)
+      i2c0_relay0_after = var.get_value()
+      print("i2c0_relay0 (after) = ", i2c0_relay0_after)
+      # for the wait timeout runtime should have increased the value
+      assert i2c0_relay0_after > i2c0_relay0_before
+
+  finally:
+    client.disconnect()
+
+if __name__ == "__main__":
+  main()

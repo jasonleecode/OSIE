7c5b30d4a59eaf23c648e85a4f4c177f81a9fa56
Ivan Tyagov <ivan@nexedi.com>
2022-04-25 15:26:37 +0300

    As now it's fully integrated no need to keep.

--- coupler/opc-ua-server/build.sh
+++ coupler/opc-ua-server/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-rm -f server_publish server_subscribe 
+rm -f server
 
 # build OPC UA server which publishes msgs
 gcc  -o server -I /usr/local/include/ -std=c99 server.c -l:libopen62541.so -L/usr/local/lib -lmbedcrypto  -lmbedx509 -I ~/open62541/src/pubsub/  -I ~/open62541/deps/
-
-
-# build OPC Ua server which subscribes to msgs
-#gcc -o server_subscribe -I /usr/local/include/ -std=c99 server_subscribe.c -l:libopen62541.so -L/usr/local/lib -I ~/open62541/src/pubsub/  -I ~/open62541/deps/
 

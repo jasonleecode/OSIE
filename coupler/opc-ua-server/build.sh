a6953d1c578e649431eac744be9a338aea56246e
Ivan Tyagov <ivan@nexedi.com>
2022-04-25 09:33:45 +0300

    Add pub/sub folders.

--- coupler/opc-ua-server/build.sh
+++ coupler/opc-ua-server/build.sh
@@ -2,7 +2,8 @@
 rm -f server_publish server_subscribe 
 
 # build OPC UA server which publishes msgs
-gcc  -o server_publish -I /usr/local/include/ -std=c99 server.c -l:libopen62541.so -L/usr/local/lib -lmbedcrypto  -lmbedx509
+gcc  -o server_publish -I /usr/local/include/ -std=c99 server.c -l:libopen62541.so -L/usr/local/lib -lmbedcrypto  -lmbedx509 -I ~/open62541/src/pubsub/  -I ~/open62541/deps/
+
 
 # build OPC Ua server which subscribes to msgs
 gcc -o server_subscribe -I /usr/local/include/ -std=c99 server_subscribe.c -l:libopen62541.so -L/usr/local/lib -I ~/open62541/src/pubsub/  -I ~/open62541/deps/

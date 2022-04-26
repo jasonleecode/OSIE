49a9ad06c608f8af9ca0540d74cd9751f4bdc128
Ivan Tyagov <ivan@nexedi.com>
2022-04-26 15:27:44 +0300

    Move to re-usable place.

--- coupler/opc-ua-server/keep_alive_publisher.h
+++ coupler/opc-ua-server/keep_alive_publisher.h
@@ -4,19 +4,6 @@ Keep alive implementation for couplers based on OPC UA's pub/sub mechanism
 
 #include "keep_alive.h"
 
-// global HEART BEATs of coupler$
-static unsigned int HEART_BEATS = 0;
-
-// the heart beat interval$
-const int DEFAULT_HEART_BEAT_INTERVAL = 250;
-static int HEART_BEAT_INTERVAL = DEFAULT_HEART_BEAT_INTERVAL;
-
-// the list of couplers onto which we depend for properly running
-unsigned int HEART_BEAT_ID_LIST[] = {0, 0, 0, 0, 0, 0, 0, 0};
-
-// the interval for publishing messages
-const int PUBLISHING_INTERVAL = 100;
-
 UA_NodeId connectionIdent, publishedDataSetIdent, writerGroupIdent;
 
 static void addPubSubConnection(UA_Server *server, UA_String *transportProfile,

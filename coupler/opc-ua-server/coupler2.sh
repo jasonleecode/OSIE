56201e256888a3c32b0730b9abbac2292263ef1c
Ivan Tyagov <ivan@nexedi.com>
2022-09-26 15:40:23 +0300

    Adjust for SR at real coupler devices.

--- coupler/opc-ua-server/coupler2.sh
+++ coupler/opc-ua-server/coupler2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-COUPLER_SR=/opt/slapgrid/f552116ea7549aa455a9739e3c361a25
+COUPLER_SR=/opt/slapgrid/834429afc45fed870654dc335be83cd4
 $COUPLER_SR/bin/server -p 4841 -m 1 -b 1 -i 2 -l 1,3 -t 100 -o 10000
--- coupler/opc-ua-server/coupler3.sh
+++ coupler/opc-ua-server/coupler3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-COUPLER_SR=/opt/slapgrid/f552116ea7549aa455a9739e3c361a25
+COUPLER_SR=/opt/slapgrid/834429afc45fed870654dc335be83cd4
 $COUPLER_SR/bin/server -p 4842 -m 1 -b 1 -i 3 -l 1,2 -t 100 -o 10000

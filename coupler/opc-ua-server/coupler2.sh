7737d1cf3b37d4b4eaecd3bf7bf7311116d7fc88
Ivan Tyagov <ivan@nexedi.com>
2022-09-26 15:34:22 +0300

    USe SlapOS's SR.

--- coupler/opc-ua-server/coupler2.sh
+++ coupler/opc-ua-server/coupler2.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./server -p 4841 -m 1 -b 1 -i 2 -l 1,3 -t 100 -o 10000
+COUPLER_SR=/opt/slapgrid/f552116ea7549aa455a9739e3c361a25
+$COUPLER_SR/bin/server -p 4841 -m 1 -b 1 -i 2 -l 1,3 -t 100 -o 10000
--- coupler/opc-ua-server/coupler3.sh
+++ coupler/opc-ua-server/coupler3.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./server -p 4842 -m 1 -b 1 -i 3 -l 1,2 -t 100 -o 10000
+COUPLER_SR=/opt/slapgrid/f552116ea7549aa455a9739e3c361a25
+$COUPLER_SR/bin/server -p 4842 -m 1 -b 1 -i 3 -l 1,2 -t 100 -o 10000

3c576535c9e7389a3d05209ff7dbfa60adb243dd
Ivan Tyagov <ivan@nexedi.com>
2022-10-05 13:55:14 +0300

    Fixes.

--- coupler/opc-ua-server/coupler1.sh
+++ coupler/opc-ua-server/coupler1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 COUPLER_SR=.
-$COUPLER_SR/server -m 1 -b 1 -i 1 -l 2,3 -t 100 -o 10000
+$COUPLER_SR/server -m 1 -b 1 -i 1 -l 2 -t 1000 -o 5000 -n opc.udp://224.0.0.22:4840/
--- coupler/opc-ua-server/keep_alive.h
+++ coupler/opc-ua-server/keep_alive.h
@@ -5,8 +5,8 @@ char *NETWORK_ADDRESS_URL_DATA_TYPE = "opc.udp://224.0.0.22:4840/";
 // global HEART BEATs of coupler
 static unsigned int HEART_BEATS = 0;
 
-// the heart beat interval$$
-const int DEFAULT_HEART_BEAT_INTERVAL = 10;
+// the heart beat interval (in ms)
+const int DEFAULT_HEART_BEAT_INTERVAL = 250;
 static int HEART_BEAT_INTERVAL = DEFAULT_HEART_BEAT_INTERVAL;
 
 // the timeout in millis after which a coupler is considered down

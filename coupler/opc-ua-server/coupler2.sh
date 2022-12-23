f526fff9154e611e6fb6c81830894012a5cbb78e
Ivan Tyagov <ivan@nexedi.com>
2022-12-23 14:45:21 +0000

    Adjust.

--- coupler/opc-ua-server/coupler2.sh
+++ coupler/opc-ua-server/coupler2.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-COUPLER_SR=.
-$COUPLER_SR/server -p 4841 -m 1 -b 1 -i 2 -l 1 -t 1000 -o 5000 -n opc.udp://224.0.0.22:4840/
+COUPLER_SR=/opt/slapgrid/016f1c79330dfc29ce1563ab3f5f1a40
+export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/opt/slapgrid/016f1c79330dfc29ce1563ab3f5f1a40/parts/open62541/lib:/opt/slapgrid/016f1c79330dfc29ce1563ab3f5f1a40/parts/mbedtls/lib'
+
+$COUPLER_SR/bin/server -p 4841 -m 1 -b 1 -i 2 -l 1 -t 1000 -o 5000 -n opc.udp://224.0.0.22:4840/

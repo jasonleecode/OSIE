3e5375e71b366a7edab241a603d4c00c14abb12c
Ivan Tyagov <ivan@nexedi.com>
2023-01-25 08:49:58 +0200

    Fixup

--- coupler/opc-ua-server/coupler1.sh
+++ coupler/opc-ua-server/coupler1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-COUPLER_SR=/opt/slapgrid/016f1c79330dfc29ce1563ab3f5f1a40
-export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/opt/slapgrid/016f1c79330dfc29ce1563ab3f5f1a40/parts/open62541/lib:/opt/slapgrid/016f1c79330dfc29ce1563ab3f5f1a40/parts/mbedtls/lib'
+COUPLER_SR=/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44
+export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44/parts/open62541/lib:/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44/parts/mbedtls/lib'
 $COUPLER_SR/bin/server -m 1 -b 1 -i 1 -l 2 -t 1000 -o 5000 -n opc.udp://224.0.0.22:4840/
--- coupler/opc-ua-server/coupler2.sh
+++ coupler/opc-ua-server/coupler2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-COUPLER_SR=/opt/slapgrid/016f1c79330dfc29ce1563ab3f5f1a40
-export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/opt/slapgrid/016f1c79330dfc29ce1563ab3f5f1a40/parts/open62541/lib:/opt/slapgrid/016f1c79330dfc29ce1563ab3f5f1a40/parts/mbedtls/lib'
+COUPLER_SR=/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44
+export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44/parts/open62541/lib:/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44/parts/mbedtls/lib'
 
 $COUPLER_SR/bin/server -p 4841 -m 1 -b 1 -i 2 -l 1 -t 1000 -o 5000 -n opc.udp://224.0.0.22:4840/

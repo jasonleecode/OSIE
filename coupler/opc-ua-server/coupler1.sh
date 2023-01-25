e48fc6d8fa1e582141789c37734bb1a1c3d3c0b4
Ivan Tyagov <ivan@nexedi.com>
2023-01-25 14:27:38 +0200

    Update virtual couplers used for tests.

--- coupler/opc-ua-server/coupler1.sh
+++ coupler/opc-ua-server/coupler1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 COUPLER_SR=/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44
 export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44/parts/open62541/lib:/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44/parts/mbedtls/lib'
-$COUPLER_SR/bin/server -m 1 -b 1 -i 1 -l 2 -t 1000 -o 5000 -n opc.udp://224.0.0.22:4840/
+./server -m 1 -i 1 -l 3 -t 10 -o 300 -n opc.udp://224.0.0.22:4840/
--- coupler/opc-ua-server/coupler3.sh
+++ coupler/opc-ua-server/coupler3.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-COUPLER_SR=.
-$COUPLER_SR/server -p 4842 -m 1 -b 1 -i 3 -l 1,2 -t 1000 -o 5000
+COUPLER_SR=/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44
+export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44/parts/open62541/lib:/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44/parts/mbedtls/lib'
+
+./server -p 4842 -m 1 -b 1 -i 3 -t 5 -o 1200 -n opc.udp://224.0.0.22:4840/

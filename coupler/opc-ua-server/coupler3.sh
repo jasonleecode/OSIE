cd417d61bbca91a12e8fdeffcca5b2d95a8ea110
Ivan Tyagov <ivan@nexedi.com>
2023-01-26 13:42:06 +0200

    Add sample setup of 3 virtual couplers where: coupler1 and coupler2 are subscribed over keep-alive network implementation over Pub/Sub to coupler3. Coupler3 is sending heart_beats every 10ms and if not heart_beat is received by coupler1,2 within 50ms they both go to "safe" mode.

--- coupler/opc-ua-server/coupler3.sh
+++ coupler/opc-ua-server/coupler3.sh
@@ -2,4 +2,4 @@
 COUPLER_SR=/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44
 export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44/parts/open62541/lib:/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44/parts/mbedtls/lib'
 
-./server -p 4842 -m 1 -b 1 -i 3 -t 5 -o 1200 -n opc.udp://224.0.0.22:4840/
+./server -p 4842 -m 1 -b 1 -i 3 -t 10 -o 50 -n opc.udp://224.0.0.22:4840/

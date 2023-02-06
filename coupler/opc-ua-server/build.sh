4a74e65bd4438505259d47e1d171173ec4daec5f
Ivan Tyagov <ivan@nexedi.com>
2023-02-06 19:18:00 +0200

    No need to keep manual way of build & run anymore.

--- coupler/opc-ua-server/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-OPEN62541_SR=/usr/local/lib
-rm -f server
-
-# build OPC UA server which publishes msgs
-gcc  -o server -I /usr/local/include/ -std=c99 server.c -l:libopen62541.so -L$OPEN62541_SR/lib -lmbedcrypto  -lmbedx509 -I ~/open62541/src/pubsub/  -I ~/open62541/deps/
-
--- coupler/opc-ua-server/coupler2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-COUPLER_SR=/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44
-export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44/parts/open62541/lib:/opt/slapgrid/6c567f8750e604c805e3a25adbbd0e44/parts/mbedtls/lib'
-
-./server -p 4841 -m 1 -i 2 -l 3 -t 10 -o 100 -n opc.udp://224.0.0.22:4840/
